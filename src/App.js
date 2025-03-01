497d951e0d87c3b75885ced84e3894be1dce4693
shahzad959 <shahzad_chauhan@hotmail.com>
2025-03-01 20:44:33 +0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import FindDoctorSearchIC from './Components/FindDoctorSearchIC/FindDoctorSearchIC';
+import DoctorCardIC from './Components/DoctorCardIC/DoctorCardIC';
+import AppointmentFromIC from './Components/AppointmentFormIC/AppointmentFormIC';
+
 import './App.css';
 
 function App() {
@@ -17,6 +21,9 @@ function App() {
                     <Route path="/signup" element={<Sign_Up />} />
                     <Route path="/login" element={<Login/>} />
                     <Route path="/instant-consultation" element={<InstantConsultation />} />
+                    <Route path="/find-doctor-search-IC" element={<FindDoctorSearchIC/>} />
+                    <Route path="/doctor-card-IC" element={<DoctorCardIC/>}/>
+                    <Route path="/appointment-form-IC" element={<AppointmentFromIC/>}/>
                 </Routes>    
                 </div>
         </Router>
